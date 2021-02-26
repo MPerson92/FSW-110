6e17a8c9d14e87d4eb1e8399b7c241245e61e5cc
MPerson92 <76800220+MPerson92@users.noreply.github.com>
2021-02-26 17:19:33 -0500

    updated basic

--- week2/basic_website_wJS/basicWebsite.js
+++ week2/basic_website_wJS/basicWebsite.js
@@ -1,5 +1,5 @@
 var header = document.createElement('h1');
-var paragrapgh = document.createElement('p');
+var paragraph = document.createElement('p');
 var unorganizedList = document.createElement('ul');
 var foot = document.createElement('footer');
 var item1 = document.createElement('li');
@@ -13,22 +13,21 @@ var navBar = document.createElement('nav');
 navLink1.href = '#';
 navLink2.href = '#';
 navLink3.href = '#';
-navLink1.textContent = " nav1 ";
-navLink2.textContent = " nav2 ";
-navLink3.textContent = " nav3 ";
-navBar.appendChild(navLink1);
-navBar.appendChild(navLink2);
-navBar.appendChild(navLink3);
+navLink1.textContent = "nav1";
+navLink2.textContent = "nav2";
+navLink3.textContent = "nav3";
+navBar.appendChild(navLink1),
+navBar.appendChild(navLink2),
+navBar.appendChild(navLink3),
 document.body.appendChild(navBar);
+
+
 
 header.innerText = "header";
 document.body.appendChild(header);
 
-paragrapgh.innerText = "paragrapgh";
-document.body.appendChild(paragrapgh);
-
-foot.textContent = "footer";
-document.body.appendChild(foot);
+paragraph.innerText = "paragraph";
+document.body.appendChild(paragraph);
 
 item1.textContent = "item 1";
 item2.textContent = "item 2";
@@ -36,6 +35,8 @@ item3.textContent = "item 3";
 unorganizedList.appendChild(item1).appendChild(item2).appendChild(item3);
 document.body.appendChild(unorganizedList);
 
+foot.textContent = "footer";
+document.body.appendChild(foot);
 
 
 
